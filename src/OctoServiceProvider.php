3ace89975d4a1e417973ab19f504c77d093ca781
Atiladanvi <atila.danvi@outlook.com>
2021-05-04 22:52:57 -0300

    Add global components

--- src/OctoServiceProvider.php
+++ src/OctoServiceProvider.php
@@ -18,11 +18,26 @@ class OctoServiceProvider extends ServiceProvider
 
         $this->loadViewsFrom(__DIR__.'/views' , 'octo');
 
+        // Octo components
         Blade::component('octo::sidebar', Sidebar::class);
         Blade::component('octo::components.hero','octo-hero');
         Blade::component('octo::components.tile','octo-tile');
         Blade::component('octo::layouts.guest','octo-guest-layout');
         Blade::component('octo::layouts.app','octo-app-layout');
+
+        // Global
+        Blade::component('octo::components.global.action-link','action-link');
+        Blade::component('octo::components.global.bitbucket-icon','bitbucket-icon');
+        Blade::component('octo::components.global.connected-account','connected-account');
+        Blade::component('octo::components.global.github-icon','github-icon');
+        Blade::component('octo::components.global.gitlab-icon','gitlab-icon');
+        Blade::component('octo::components.global.facebook-icon','facebook-icon');
+        Blade::component('octo::components.global.facebook-icon','linked-in-icon');
+        Blade::component('octo::components.global.google-icon','google-icon');
+        Blade::component('octo::components.global.twiter-icon','twiter-icon');
+        Blade::component('octo::components.global.socialstream','socialstream-providers');
+
+        // Livewire
         Livewire::component('octo::guest-navigation-menu', GuestNavigationMenu::class);
     }
 
