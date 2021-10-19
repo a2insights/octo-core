f6c534df5d7edb8ed18f44fd81f128adbef5278f
Atiladanvi <atila.danvi@outlook.com>
2021-10-19 01:05:17 -0300

    Depreciates helper octo_action

--- src/helpers.php
+++ src/helpers.php
@@ -1,17 +1,5 @@
 <?php
 
-if (!function_exists('___')) {
-    /**
-     * Compose translation
-     *
-     * @param array $values
-     * @return string
-     */
-    function ___(...$values) {
-       return collect($values)->map(fn($v) => __($v))->implode(' ');
-    }
-}
-
 if (!function_exists('octo_route')) {
     /**
      * Return the octo_route
@@ -25,19 +13,5 @@ if (!function_exists('octo_route')) {
             'name' => $name,
             'params' => $params
         ]);
-    }
-}
-
-if (!function_exists('octo_action')) {
-    /**
-     * Factory octo action
-     *
-     * @param $name
-     * @param $params
-     * @return \Octo\Resources\Quasar\Action
-     */
-    function octo_action($name, $params)
-    {
-        return (new \Octo\Resources\Quasar\Action(octo_route($name, $params)));
     }
 }
