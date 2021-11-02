9eb5b4bcbd9789021587b7390a6d18d7267df5db
Atiladanvi <atila.danvi@outlook.com>
2021-11-02 20:52:04 -0300

    Upgrades from laravel/jestream

--- stubs/resources/views/notifications/index.blade.php
+++ stubs/resources/views/notifications/index.blade.php
@@ -1,11 +1,11 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-purple-500 leading-tight">
-            Notifications
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Notifications') }}
         </h2>
     </x-slot>
 
     <div class="mt-1 flex flex-col">
-        @livewire('list-notifications')
+        @livewire('octo-list-notifications')
     </div>
 </x-app-layout>
