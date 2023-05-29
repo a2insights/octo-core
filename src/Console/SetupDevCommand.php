9b6c6c6e809a3d0cdcce7b6a8581f8bfd242f13f
a21ns1g4ts <atila.danvi@outlook.com>
2023-05-29 09:49:59 -0300

    Wip dev setup

--- src/Console/SetupDevCommand.php
+++ src/Console/SetupDevCommand.php
@@ -12,35 +12,15 @@ class SetupDevCommand extends Command
 
     protected $description = 'Dev aplication';
 
-    public const DEFAULT_SUPER_ADMIN_NAME = 'Octo Super Administrator';
-
-    public const DEFAULT_SUPER_ADMIN_EMAIL = 'super-admin@octo.dev';
-
-    public const DEFAULT_SUPER_ADMIN_PASSWORD = 'octoSuperAdmin';
-
-    public const DEFAULT_ADMIN_NAME = 'Octo Administrator';
-
-    public const DEFAULT_ADMIN_EMAIL = 'admin@octo.dev';
-
-    public const DEFAULT_ADMIN_PASSWORD = 'octoAdmin';
-
-    public const DEFAULT_USER_NAME = 'Octo User';
+    public const DEFAULT_USER_NAME = 'user';
 
     public const DEFAULT_USER_EMAIL = 'user@octo.dev';
 
-    public const DEFAULT_USER_PASSWORD = 'octoUser';
+    public const DEFAULT_USER_PASSWORD = '123456';
 
     public function handle()
     {
         $this->call('migrate:fresh', ['--force' => true, '--seed' => true]);
-
-        $this->info('Creating super admin user');
-        $admin = $this->setUpSuperAdminAccount();
-        $this->info('Super admin user created');
-
-        $this->info('Creating admin user');
-        $admin = $this->setUpAdminAccount();
-        $this->info('Admin user created');
 
         $this->info('Creating user account');
         $user = $this->setUpUserAccount();
@@ -58,36 +38,6 @@ class SetupDevCommand extends Command
         $user->markEmailAsVerified();
 
         $this->comment(sprintf('Log in user with email %s and password %s', self::DEFAULT_USER_EMAIL, self::DEFAULT_USER_PASSWORD));
-
-        return $user;
-    }
-
-    private function setUpAdminAccount()
-    {
-        $user = User::forceCreate([
-            'name' => self::DEFAULT_ADMIN_NAME,
-            'email' => self::DEFAULT_ADMIN_EMAIL,
-            'password' => Hash::make(self::DEFAULT_ADMIN_PASSWORD),
-        ]);
-
-        $user->markEmailAsVerified();
-
-        $this->comment(sprintf('Log in admin with email %s and password %s', self::DEFAULT_ADMIN_EMAIL, self::DEFAULT_ADMIN_PASSWORD));
-
-        return $user;
-    }
-
-    private function setUpSuperAdminAccount()
-    {
-        $user = User::forceCreate([
-            'name' => self::DEFAULT_SUPER_ADMIN_NAME,
-            'email' => self::DEFAULT_SUPER_ADMIN_EMAIL,
-            'password' => Hash::make(self::DEFAULT_SUPER_ADMIN_PASSWORD),
-        ]);
-
-        $user->markEmailAsVerified();
-
-        $this->comment(sprintf('Log in seper admin with email %s and password %s', self::DEFAULT_SUPER_ADMIN_EMAIL, self::DEFAULT_SUPER_ADMIN_PASSWORD));
 
         return $user;
     }
