af16117240e5df98000406128b2b7d7a1ecc2307
a21ns1g4ts <atila.danvi@outlook.com>
2024-02-11 01:24:18 -0300

    Update packages and fix wrong import

--- src/User/UserServiceProvider.php
+++ src/User/UserServiceProvider.php
@@ -2,14 +2,13 @@
 
 namespace Octo\User;
 
-use App\Models\User;
 use Filament\Facades\Filament;
 use Illuminate\Support\Facades\Route;
 use Livewire\Livewire;
+use Octo\Octo;
 use Octo\User\Filament\Components\Phone;
 use Octo\User\Filament\Components\Username;
 use Octo\User\Filament\Pages\BannedUser;
-use Octo\User\Filament\Pages\Login;
 use Octo\User\Filament\Pages\Register;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
@@ -18,10 +17,9 @@ class UserServiceProvider extends PackageServiceProvider
 {
     public function bootingPackage(): void
     {
-        User::observe(UserObserver::class);
+        Octo::getUserModel()::observe(UserObserver::class);
 
         Livewire::component('BannedUser', BannedUser::class);
-        Livewire::component('Login', Login::class);
         Livewire::component('Register', Register::class);
         Livewire::component('phone', Phone::class);
         Livewire::component('username', Username::class);
