affd2966e46ff57a3037b4e2ea6adcbbe180532a
a21ns1g4ts <atila.danvi@outlook.com>
2025-01-20 23:04:11 -0300

    Wip stubed files

--- config/filament-saas.php
+++ config/filament-saas.php
@@ -2,13 +2,13 @@
 
 return [
     'users' => [
-        'model' => A2Insights\FilamentSaas\User\User::class,
+        'model' => App\Models\User::class,
         'resource' => A2Insights\FilamentSaas\User\Filament\UserResource::class,
         'tenant_scope' => false,
     ],
 
     'companies' => [
-        'model' => A2Insights\FilamentSaas\Tenant\Company::class,
+        'model' => App\Models\Company::class,
     ],
 
     'terms_of_service_url' => 'terms-of-service',
